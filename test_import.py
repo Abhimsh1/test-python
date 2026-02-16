c0e1116cf5e6743b8d33735b4835cb38c9febefe
abhilash mohan <abhilash.mohan@bti.bmwgroup.com>
2026-02-16 17:14:48 +0530

    ttt

--- test_import.py
+++ test_import.py
@@ -1,6 +1,12 @@
 from test_class import TestClass
 from os import path, getcwd
 
-print("Current working directory:", getcwd())
+print("Current working directory:", getcwd("aa"))
 print("Path to test_class.py:", path.abspath("test_class.py"))
+
+
+class TestClass(TestClass):
+    def test_method(self):
+        print("Implementation of test_method")
+
 obj = TestClass()
