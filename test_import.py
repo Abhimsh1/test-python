be2d7fce0da62e3d87b72b432b4dc9ec9fc1f204
Abhilash Abhimsh <108720286+Abhimsh1@users.noreply.github.com>
2026-02-16 17:36:33 +0530

    Update test_import.py
    Co-authored-by: propel-code-bot[bot] <203372662+propel-code-bot[bot]@users.noreply.github.com>

--- test_import.py
+++ test_import.py
@@ -1,7 +1,7 @@
 from test_class import TestClass
 from os import path, getcwd
 
-print("Current working directory:", getcwd("aa"))
+print("Current working directory:", getcwd())
 print("Path to test_class.py:", path.abspath("test_class.py"))
 
 
